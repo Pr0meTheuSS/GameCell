c45f4c499ed6acc445c1a701e2f0235d6ba6c423
u_olimpiev <y.olimpiev@g.nsu.ru>
2021-04-12 19:29:18 +0700

    v0.1

---
+++ Cell/Cell/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Cell
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.Location = new System.Drawing.Point(280, 256);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(38, 38);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 368);
+            this.Controls.Add(this.pictureBox1);
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Клетка-Меню";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Cell/Cell/Mob.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cell
+{
+    class Mob
+    {
+        int x;
+        int y;
+        int x_velocity;
+        int y_velocity;
+        int win_size_x;
+        int win_size_y;
+
+        public Mob(List<int> position, List<int> velocity, List<int> winSize){
+            this.x = position[0];
+            this.y = position[1];
+            this.x_velocity = velocity[0];
+            this.y_velocity = velocity[1];
+            this.win_size_x = winSize[0];
+            this.win_size_y = winSize[1];
+        }
+
+        public void Move() {
+           // отражение от стен (вычитание 45 от win_size костыль, но иначе моб выпадает за пределы окна)
+            if (this.x <= 0 || this.x >= this.win_size_x - 45) {
+                this.x_velocity *= -1;
+            }
+            if (this.y <= 0 || this.y >= this.win_size_y - 45)
+            {
+                this.y_velocity *= -1;
+            }
+
+            this.x += this.x_velocity;
+            this.y += this.y_velocity;
+        }
+
+        public List<int> GetPosition() {
+            List<int> ret = new List<int>();
+            ret.Add(this.x);
+            ret.Add(this.y);
+            return ret;
+        }
+    }
+}
